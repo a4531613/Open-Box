07ed08e1c9c71c6c247005e6663ce1abcba97700
a4531613 <g465884297@gmail.com>
2026-09-09 02:14:47 +0800

    privacy: stop automatic UI release checks

--- panel/src/views/HomePage.vue
+++ panel/src/views/HomePage.vue
@@ -58,7 +58,6 @@
 import { fetchBackendVersion } from '@/api'
 import SideBar from '@/components/sidebar/SideBar.vue'
 import { dockTop } from '@/composables/paddingViews'
-import { useSettings } from '@/composables/settings'
 import { useSwipeRouter } from '@/composables/swipe'
 import { ROUTE_ICON_MAP } from '@/constant'
 import { renderRoutes } from '@/helper'
@@ -102,8 +101,4 @@ watch(documentVisible, () => {
   if (documentVisible.value !== 'visible') return
   fetchProxies()
 })
-
-const { checkUIUpdate } = useSettings()
-
-checkUIUpdate()
 </script>
